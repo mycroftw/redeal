02c5ce52c875219386a304f20fcde090bec45a17
mycroftw <30244806+mycroftw@users.noreply.github.com>
2025-11-19 11:59:14 -0600

    Fix: move commonly used ideas out of specific file.
    1. `Pass` dataclass and "create the list of lists of hands"
       moved to `generate_hands.py`.
    2. `marmic` and other tests moved to `smp_definitions.py`.

--- mfscripts/smp/smp_marmic.py
+++ mfscripts/smp/smp_marmic.py
@@ -1,23 +1,23 @@
 """SMP: Strong hands and 4441s"""
 
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Optional
 
-from mfscripts.smp.smp_definitions import (
-    combine_and_print_hands,
-    generate_pbn_hands,
+from redeal import Deal, SmartStack, hcp
+
+from generate_hands import Pass, combine_and_print_hands, generate_pbn_passes
+from smp_definitions import (
+    marmic,
     ns_hcp,
     one_club_opener,
+    one_diamond_response_sc,
+    strong_response_sc,
     two_diamond_shape,
     two_diamonds_opener,
 )
-from redeal import Deal, Shape, SmartStack, hcp
 
+# TWEAK HERE
 DEBUG = False
-marmic = Shape("(4441)")  # any marmic hand
-OUTPUTS = []
-MIN_HCP_STRONG = 29
+MIN_HCP_STRONG = 23
 
 
 def two_diamond_accept(deal: Deal) -> bool:
@@ -31,7 +31,11 @@ def two_diamond_accept(deal: Deal) -> bool:
 
 def one_club_marmic(deal: Deal) -> bool:
     """Accept if 1C -> GF and marmic"""
-    if one_club_opener(deal.south) and marmic(deal.south) and hcp(deal.north) > 7:
+    if (
+        one_club_opener(deal.south)
+        and marmic(deal.south)
+        and not one_diamond_response_sc(deal.north)
+    ):
         if DEBUG:
             print(deal)
         return True
@@ -40,7 +44,7 @@ def one_club_marmic(deal: Deal) -> bool:
 
 def one_club_strong_resp(deal: Deal) -> bool:
     """Accept if 1C and resp >= 12"""
-    if one_club_opener(deal.south) and hcp(deal.north) > 11:
+    if one_club_opener(deal.south) and strong_response_sc(deal.north):
         if DEBUG:
             print(deal)
         return True
@@ -48,17 +52,12 @@ def one_club_strong_resp(deal: Deal) -> bool:
 
 
 def one_club_strong_marmic(deal: Deal) -> bool:
+    """Accept if 1C and responder is 4441 >= 12"""
     if one_club_strong_resp(deal) and marmic(deal.north):
         if DEBUG:
             print(deal)
         return True
     return False
-
-
-@dataclass
-class Pass:
-    accept_function: Callable[[Deal], bool]
-    predeal: Optional[dict[str, SmartStack]]
 
 
 passes = (
@@ -68,10 +67,8 @@ passes = (
     Pass(one_club_strong_marmic, {"N": SmartStack(marmic, hcp, range(12, 25))}),
 )
 
-
-for pass_ in passes:
-    OUTPUTS.append(generate_pbn_hands(pass_.accept_function, pass_.predeal, 10))
+outputs = generate_pbn_passes(passes)
 
 F = "marmic.pbn"
-with (Path.cwd() / F).open(encoding="utf-8", mode="w") as f:
-    combine_and_print_hands(f, OUTPUTS, randomize=True, alternate_after=5)
+with (Path.cwd() / "pbn" / F).open(encoding="utf-8", mode="w") as f:
+    combine_and_print_hands(f, outputs, randomize=True, alternate_after=5)
